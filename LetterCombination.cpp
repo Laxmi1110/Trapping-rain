287164365d33386f0731decce2e0f55e5b96e64f
Laxmi Gupta <laxmigupta188777@gmail.com>
2026-03-07 20:26:27 +0530

    Add letter combinations generation using backtracking
    Implement backtracking to generate letter combinations from phone digits.

---
+++ LetterCombination.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> result;
+
+    void backtrack(string digits, int index, string current, vector<string> &map){
+        if(index == digits.size()){
+            result.push_back(current);
+            return;
+        }
+
+        string letters = map[digits[index] - '0'];
+
+        for(char c : letters){
+            backtrack(digits, index + 1, current + c, map);
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty()) return {};
+
+        vector<string> map = {
+            "", "", "abc", "def", "ghi",
+            "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+
+        backtrack(digits, 0, "", map);
+        return result;
+    }
+};
